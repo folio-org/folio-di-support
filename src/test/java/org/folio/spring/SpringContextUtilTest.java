0c828cb1b5fcdb45d2d75dd1020dda955a23cc83
Pavlo Smahin <Pavlo_Smahin@epam.com>
2025-11-14 13:00:20 +0200

    feat(deps): upgrade the module for Vert.X 5.0.x (#52)
    Closes: FDIS-38

--- src/test/java/org/folio/spring/SpringContextUtilTest.java
+++ src/test/java/org/folio/spring/SpringContextUtilTest.java
@@ -1,17 +1,20 @@
 package org.folio.spring;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Context;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
 import org.folio.spring.config.TestConfiguration2;
 import org.folio.util.TestAutowireTarget;
 import org.folio.util.TestConfiguration;
 import org.junit.Before;
 import org.junit.Test;
 
-import io.vertx.core.Context;
-import io.vertx.core.Vertx;
-
 public class SpringContextUtilTest {
+
   private Vertx vertx;
   private Context context;
 
@@ -65,5 +68,41 @@ public class SpringContextUtilTest {
     SpringContextUtil.autowireDependencies(autowireTarget, context);
 
     assertEquals(TestConfiguration2.BEAN_VALUE, autowireTarget.getTestBean().getValue());
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void shouldThrowExceptionWhenConfigurationClassNotFound() {
+    context.config().put("spring.configuration", "org.folio.NonExistentConfiguration");
+    SpringContextUtil.init(vertx, context);
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void shouldThrowExceptionWhenSpringContextNotInitialized() {
+    TestAutowireTarget autowireTarget = new TestAutowireTarget();
+    SpringContextUtil.autowireDependencies(autowireTarget, context);
+  }
+
+  @Test
+  public void shouldAutowireDependenciesFromFirstContext() throws Exception {
+    DeploymentOptions options = new DeploymentOptions().setConfig(context.config());
+
+    vertx.deployVerticle(new AbstractVerticle() {
+      @Override
+      public void start() {
+        SpringContextUtil.init(vertx, context, TestConfiguration.class);
+      }
+    }, options).toCompletionStage().toCompletableFuture().get();
+
+    TestAutowireTarget autowireTarget = new TestAutowireTarget();
+    SpringContextUtil.autowireDependenciesFromFirstContext(autowireTarget, vertx);
+
+    assertNotNull(autowireTarget.getTestBean());
+    assertEquals(TestConfiguration.BEAN_VALUE, autowireTarget.getTestBean().getValue());
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void shouldThrowExceptionWhenNoSpringContextInVertx() {
+    TestAutowireTarget autowireTarget = new TestAutowireTarget();
+    SpringContextUtil.autowireDependenciesFromFirstContext(autowireTarget, vertx);
   }
 }
